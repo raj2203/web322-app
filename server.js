e6acbf9d588c3df12ff0179ca2ff84785863e115
Your Name <you@example.com>
2022-02-04 20:59:33 -0500

    update

--- server.js
+++ server.js
@@ -1,14 +1,17 @@
+var HTTP_PORT = process.env.PORT || 8080;
 var express = require("express");
 var app = express();
+const path = require("path");
+app.use(express.static('static'));
 
-var HTTP_PORT = process.env.PORT || 8080;
+
 
 // call this function after the http server starts listening for requests
 function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static('public'));
+
 
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
@@ -16,8 +19,8 @@ app.get("/", function(req,res){
 });
 
 // setup another route to listen on /about
-app.get("/about", function(req,res){
-  res.sendFile(__dirname, + "/about.html");
+app.get("/sign_in.html", function(req, res){
+  res.sendFile(path.join(dirname, "/views/about.html"));
 });
 
 // setup http server to listen on HTTP_PORT
