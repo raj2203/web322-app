1945186a81573edcf6a0534345e74d35d82f6d5a
Your Name <you@example.com>
2022-02-04 20:10:37 -0500

    update

--- server.js
+++ server.js
@@ -8,16 +8,14 @@ function onHttpStart() {
   console.log("Express http server listening on: " + HTTP_PORT);
 }
 
-app.use(express.static('public'));
-
 // setup a 'route' to listen on the default url path (http://localhost)
 app.get("/", function(req,res){
-    res.redirect("/about");
+    res.send("Hello World<br /><a href='/about'>Go to the about page</a>");
 });
 
 // setup another route to listen on /about
 app.get("/about", function(req,res){
-  res.sendFile(__dirname, + "/views/about.html");
+    res.send("<h3>About</h3>");
 });
 
 // setup http server to listen on HTTP_PORT
