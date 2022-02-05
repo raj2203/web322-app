f5ad8f42c8ab4b5489a7d9dfd201a4dc059e2552
Your Name <you@example.com>
2022-02-04 22:43:37 -0500

    update

---
+++ blog-service.js
@@ -0,0 +1,53 @@
+const e = require("express");
+const fs = require("fs");
+//const { resolve } = require("path");
+var posts = [];
+var categories = [];
+
+module.exports.initialize = function(){
+    return new Promise((resolve,reject)=>{
+        fs.readFile('./data/posts.json',(err,data)=>{
+            if(err){
+                reject(err);
+            }
+            posts = JSON.parse(data);
+            //resolve();
+        });
+        fs.readFile('./data/categories.json',(err,data)=>{
+            if(err){
+                reject(err);
+            }
+            categories = JSON.parse(data);
+        })
+        resolve();
+    });
+}
+module.exports.getAllPosts = function(){
+    return new Promise((resolve,reject)=>{
+        if(posts.length ==0){
+            reject("No Post Avaliable");
+        }
+        resolve(posts);
+    })
+}
+module.exports.getPublishedPosts = function(){
+    return new Promise((resolve,reject)=>{
+        var post =[];
+        for(let i=0;i<posts.length;i++){
+            if(posts[i].isPost){
+                post.push(posts[i]);
+            }
+        }
+        if(post.length==0) reject("No Public Post Found");
+
+        resolve(post);
+    })
+}
+module.exports.getCategories = function(){
+    return new Promise((resolve,reject)=>{
+        if(category.length==0){
+            reject("No Catagories Found")
+        }
+        resolve(category);
+    })
+}
--- server.js
+++ server.js
@@ -1,27 +1,59 @@
-var HTTP_PORT = process.env.PORT || 8080;
-var express = require("express");
+var data = require('./blog-service.js');
+var path = require("path");
+var express = require('express');
 var app = express();
-const path = require("path");
-app.use(express.static('static'));
 
+const { join } = require("path");
+app.use(express.static('public'));
 
+var HTTP_PORT = process.env.PORT || 8080;
 
-// call this function after the http server starts listening for requests
-function onHttpStart() {
-  console.log("Express http server listening on: " + HTTP_PORT);
+function onHttpStart(){
+    console.log("Express http server listening on PORT: " + HTTP_PORT);
 }
-
-
-
-// setup a 'route' to listen on the default url path (http://localhost)
+//setup a route to listen on the default url path
 app.get("/", function(req,res){
-    res.redirect('/about');
+    res.redirect("/about");
+});
+//setup another route to listen in /about
+app.get("/about", function(req,res){
+    res.sendFile(path.join(__dirname,"/views/about.html"));
+});
+//setup a route to listen in /blogs
+app.get("/blogs", function(req,res){
+  data.getAllPosts()
+  .then((data)=>{
+      res.json(data);
+  }).catch((err) => {
+      res.json("{message: "+err+'}');
+      })
+});
+//setup a route to listen in /posts
+app.get("/managers", function(req,res){
+  data.getPublishedPosts()
+  .then((data)=>{
+      res.json(data);
+  }).catch((err) => {
+      res.json("{message: "+err+'}');
+  })
+});
+//setup a route to listen in /categories
+app.get("/departments", function(req,res){
+  data.getCategories()
+  .then((data)=>{
+      res.json(data);
+  }).catch((err) => {
+      res.json("{message: "+err+'}');
+  })
+});
+//This must be the last call, otherwise the files called after won't execute.
+app.use((req,res)=>{
+  res.status(404).sendFile(path.join(__dirname,"/views/404.html"));
 });
 
-// setup another route to listen on /about
-app.get("/sign_in.html", function(req, res){
-  res.sendFile(path.join(dirname, "/views/about.html"));
-});
 
-// setup http server to listen on HTTP_PORT
-app.listen(HTTP_PORT, onHttpStart);
+data.initialize().then(function(){ //before I run my server I initialize the data
+  app.listen(HTTP_PORT, onHttpStart);
+}).catch(function(err){
+  console.log("Unable to start server: "+err);
+});
